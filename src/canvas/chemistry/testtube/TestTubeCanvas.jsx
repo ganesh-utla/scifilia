71689824dec359aa0a69d911061ed94d19fc73fc
Ganesh Utla <ganeshutla888@gmail.com>
2024-03-27 12:48:43 +0530

    some minor changes

--- src/canvas/chemistry/testtube/TestTubeCanvas.jsx
+++ src/canvas/chemistry/testtube/TestTubeCanvas.jsx
@@ -8,7 +8,7 @@ import { Suspense } from "react";
 import CanvasLoader from "../../CanvasLoader";
 import TestTube from "./TestTube";
 
-const TestTubeCanvas = () => {
+const TestTubeCanvas = ({ isExplorePage = false }) => {
 
   return (
     <Canvas className="cursor-grab active:cursor-grabbing ml-auto text-right" camera={{
@@ -20,12 +20,14 @@ const TestTubeCanvas = () => {
       <ambientLight />
       <Environment preset="sunset" />
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          autoRotate
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
+        {isExplorePage ? <OrbitControls /> :
+          <OrbitControls
+            autoRotate
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+        }
         <TestTube />
         <Preload all/>
       </Suspense>
